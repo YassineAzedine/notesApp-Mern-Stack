ce3459b07345d60ffee005e51aff02e438e5d16e
YassineAzedine <77061498+YassineAzedine@users.noreply.github.com>
2022-02-05 18:30:11 +0100

    ajouter front end

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
+import Notes from './components/Notes'
+import AddNotes from './components/AddNotes'
+import EditNote from './components/EditNotes'
+
+
+function App() {
+  return (
+   <div>
+     hello world
+   </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/EditNotes.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditNotes extends Component {
+    render() {
+        return (
+            <div>
+                edit notes
+            </div>
+        );
+    }
+}
+
+export default EditNotes;
